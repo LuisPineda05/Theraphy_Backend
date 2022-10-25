6cc8091a3249eb6ec399a809b38ae305247a930e
DiegoDeLaCruz <u20201c334@upc.edu.pe>
2022-10-25 12:01:26 -0500

    Initial Commit

---
+++ src/main/java/com/theraphy/backendtheraphy/BackendTheraphyApplication.java
@@ -0,0 +1,13 @@
+package com.theraphy.backendtheraphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTheraphyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTheraphyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/theraphy/backendtheraphy/BackendTheraphyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theraphy.backendtheraphy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTheraphyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
